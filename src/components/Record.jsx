8abc61915fea0e6a28cf5b4a6ff089ef055274d0
Aditya Kumar <araj7060@gmail.com>
2022-06-11 01:11:22 +0530

    updated style

--- src/components/Record.jsx
+++ src/components/Record.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React from "react";
 
-import '../css/record.css';
-import useStore from '../store';
+import "../css/record.css";
+import useStore from "../store";
 
-export default function Record({title, id}) {
-    const setSelectedRecord = useStore(state => state.setSelectedRecord);
-  return (
-    <div className="record" onClick={e => setSelectedRecord(id)}>
-        {title}
-    </div>
-  )
+export default function Record({ title, id }) {
+    const setSelectedRecord = useStore((state) => state.setSelectedRecord);
+    const selectedRecord = useStore((state) => state.selectedRecord);
+    return (
+        <div
+            className="record"
+            onClick={(e) => setSelectedRecord(id)}
+            style={{
+                ...(selectedRecord._id === id && {backgroundColor: 'white'}),
+            }}
+        >
+            {title}
+        </div>
+    );
 }
--- src/store.js
+++ src/store.js
@@ -14,43 +14,67 @@ const useStore = create((set) => ({
     records: [
         {
             _id: "1",
-            title: "friendly invitation",
-            text: "hello guys, i am iron-man, your best friend, please hangout with me at facebook live",
+            title: "1. Lorem ipsum dolor",
+            text: "Elinor lee, a gangster's moll living in the Harlem section of New york City, has signed up-and-coming boxer Benny Blue to a 10-year contract. Lee and a pair of corrupt fight promoter scheeme to build up Blue as a potential champoin, with the goal of betting against him  when they force him to take a dive in a champion fight.",
         },
         {
             _id: "2",
-            title: "Facebook's take on Global warming",
-            text: "'Global warming' is a phrase that refers to the effect on the climate of human activities, in particular the burning of fossil fuels (coal, oil and gas) and large-scale deforestation, which cause emissions to the atmosphere of large amounts of 'greenhouse gases', of which the most important is carbon dioxide. Such gases absorb infrared radiation emitted by the Earth's surface and act as blankets over the surface keeping it warmer than it would otherwise be. Associated with this warming are changes of climate. The basic science of the 'greenhouse effect' that leads to the warming is well understood. More detailed understanding relies on numerical models of the climate that integrate the basic dynamical and physical equations describing the complete",
+            title: "2. Facebook's take on Global warming",
+            text: "'Global warming' is a phrase that refers to the effect on the climate of human activities, in particular the burning of fossil fuels (coal, oil and gas) and large-scale deforestation, which cause emissions to the atmosphere of large amounts of 'greenhouse gases', of which the most important is carbon dioxide. Such gases absorb infrared radiation emitted by the Earth's surface and act as blankets over the surface keeping it warmer than it would otherwise be. Associated with this warmcing are changes of climate. The basic science of the 'greenhouse effect' that leads to the warming is well understood. More detailed understanding relies on numerical models of the climate that integrate the basic dynamical and physical equations describing the complete",
         },
     ],
     tokenData: {
-        person: ["aditya", "abhishek", "rahul", 'vivek', 'john', 'neon', 'human'],
-        org: ["facebook", 'instagram', 'apple', 'meta', 'greenhouse'],
+        person: [
+            "aditya",
+            "abhishek",
+            "rahul",
+            "vivek",
+            "john",
+            "neon",
+            "human",
+            "Elinor Lee",
+            "Benny Blue",
+            "Lee",
+            "Blue",
+            "Fredi",
+        ],
+        org: [
+            "facebook",
+            "instagram",
+            "apple",
+            "meta",
+            "greenhouse",
+            "New York City",
+        ],
     },
 
     selectedRecord: {
         _id: "1",
-        title: "title",
-        text: "hello guys, i am aditya, your best friend, please hangout with me at facebook live",
+        title: "1. Lorem ipsum dolor",
+        text: "Elinor lee, a gangster's moll living in the Harlem section of New york City, has signed up-and-coming boxer Benny Blue to a 10-year contract. Lee and a pair of corrupt fight promoter scheeme to build up Blue as a potential champoin, with the goal of betting against him  when they force him to take a dive in a champion fight.",
     },
 
-    setSelectedRecord:  (_id) => {
+    setSelectedRecord: (_id) => {
         set((state) => ({
-            selectedRecord: state.records.find((record) => record._id === _id)
+            selectedRecord: state.records.find((record) => record._id === _id),
         }));
     },
-
 
     removeAnotation: (index, word) => {
         set((state) => {
             const target = state.selectedRecord;
-            target.text = target.text.slice(0, index) + target.text.slice(index+word.length);
+            target.text =
+                target.text.slice(0, index) +
+                target.text.slice(index + word.length);
 
             return {
-                records: [...state.records.filter((el) => el._id !== target._id), target]
-            }
-        })
-    }
+                records: [
+                    ...state.records.filter((el) => el._id !== target._id),
+                    target,
+                ],
+            };
+        });
+    },
 }));
 
 export default useStore;
